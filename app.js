6904fc5dd0d10e94c6e79e7d3df91f55012355d8
Eric Fuller <efuller82@gmail.com>
2019-11-19 13:49:23 -0600

    fixed passport.js; app works

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const flash = require('connect-flash');
 const session = require('express-session');
-const passport = require('passport');
 
 const app = express();
 
@@ -23,7 +23,7 @@ app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
 // Bodyparser
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // Express Session
 app.use(session({
@@ -41,7 +41,7 @@ app.use(flash());
 
 
 // Global Vars
-app.use((req, res, next) => {
+app.use(function (req, res, next) {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
@@ -49,8 +49,8 @@ app.use((req, res, next) => {
 });
 
 //Routes
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
 
 const PORT = process.env.PORT || 5000;
 
